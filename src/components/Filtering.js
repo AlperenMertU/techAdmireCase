fe616945bceea25d498f966792822f695322858e
Alperen M. Urdoğan <89020497+AlperenMertU@users.noreply.github.com>
2024-03-25 13:00:36 +0300

    pagionation is ok

--- src/components/Filtering.js
+++ src/components/Filtering.js
@@ -1,16 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import userData from "../usersData/users.json";
-import { setFilteredUsers } from '../components/DataSlice'; 
+import { setFilteredUsers } from '../components/DataSlice';
 
 const Filtering = () => {
   const [university, setUniversity] = useState('');
   const [language, setLanguage] = useState('');
   const [country, setCountry] = useState('');
+  const [duration, setDuration] = useState('');
   const [minCost, setMinCost] = useState('');
   const [maxCost, setMaxCost] = useState('');
+  const [uniqueCountries, setUniqueCountries] = useState([]);
+  const [uniqueDurations, setUniqueDurations] = useState([]);
+  const [uniqueUniversities, setUniqueUniversities] = useState([]);
 
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    const countries = userData.map(user => user.ülke);
+    const uniqueCountries = [...new Set(countries)];
+    setUniqueCountries(uniqueCountries);
+
+    const durations = userData.map(user => user.süre);
+    const uniqueDurations = [...new Set(durations)];
+    setUniqueDurations(uniqueDurations);
+
+    const universities = userData.map(user => user.univercity);
+    const uniqueUniversities = [...new Set(universities)];
+    setUniqueUniversities(uniqueUniversities);
+  }, []);
 
   const handleUniversityChange = (e) => {
     setUniversity(e.target.value);
@@ -24,6 +42,10 @@ const Filtering = () => {
     setCountry(e.target.value);
   };
 
+  const handleDurationChange = (e) => {
+    setDuration(e.target.value);
+  };
+
   const handleMinCostChange = (e) => {
     setMinCost(e.target.value);
   };
@@ -33,21 +55,60 @@ const Filtering = () => {
   };
 
   const handleFilterClick = () => {
-    const filteredUsers = userData.filter(user => 
+    let filteredUsers = userData.filter(user =>
       user.univercity.toLowerCase().includes(university.toLowerCase()) &&
       user.dil.toLowerCase().includes(language.toLowerCase()) &&
       user.ülke.toLowerCase().includes(country.toLowerCase()) &&
-      parseFloat(user.maliyet.replace(",", "")) >= parseFloat(minCost) &&
-      parseFloat(user.maliyet.replace(",", "")) <= parseFloat(maxCost)
+      user.süre.toLowerCase().includes(duration.toLowerCase())
     );
-    dispatch(setFilteredUsers(filteredUsers)); 
+
+    if (minCost !== '' && !isNaN(parseFloat(minCost))) {
+      filteredUsers = filteredUsers.filter(user =>
+        parseFloat(user.maliyet.replace(' TL', '').replace(',', '')) >= parseFloat(minCost)
+      );
+    }
+
+    if (maxCost !== '' && !isNaN(parseFloat(maxCost))) {
+      filteredUsers = filteredUsers.filter(user =>
+        parseFloat(user.maliyet.replace(' TL', '').replace(',', '')) <= parseFloat(maxCost)
+      );
+    }
+
+    dispatch(setFilteredUsers(filteredUsers));
   };
-  
+
   return (
     <div>
-      <input type="text" placeholder="Üniversite Adı" value={university} onChange={handleUniversityChange} />
-      <input type="text" placeholder="Dil" value={language} onChange={handleLanguageChange} />
-      <input type="text" placeholder="Ülke" value={country} onChange={handleCountryChange} />
+
+    
+    <select value={university} onChange={handleUniversityChange}>
+    <option value="">Tüm Üniversiteler</option>
+    {uniqueUniversities.map((university, index) => (
+      <option key={index} value={university}>{university}</option>
+    ))}
+  </select>
+
+      <select value={language} onChange={handleLanguageChange}>
+        <option value="">Tüm Diller</option>
+        <option value="İngilizce">İngilizce</option>
+        <option value="Fransızca">Fransızca</option>
+        <option value="Türkçe">Türkçe</option>
+      </select>
+
+      <select value={country} onChange={handleCountryChange}>
+        <option value="">Tüm Ülkeler</option>
+        {uniqueCountries.map((country, index) => (
+          <option key={index} value={country}>{country}</option>
+        ))}
+      </select>
+
+      <select value={duration} onChange={handleDurationChange}>
+        <option value="">Tüm Süreler</option>
+        {uniqueDurations.map((duration, index) => (
+          <option key={index} value={duration}>{duration} Yıl</option>
+        ))}
+      </select>
+
       <input type="number" placeholder="Min Maliyet" value={minCost} onChange={handleMinCostChange} />
       <input type="number" placeholder="Max Maliyet" value={maxCost} onChange={handleMaxCostChange} />
 
--- src/components/userTable.js
+++ src/components/userTable.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import userJson from "../usersData/users.json";
 
 import { useSelector } from 'react-redux';
@@ -6,14 +6,26 @@ import { selectFilteredUsers } from '../components/DataSlice';
 
 const UserTable = () => {
 
+  const perPage = 6; 
+  const [currentPage, setCurrentPage] = useState(1); 
   const filteredUsers = useSelector(selectFilteredUsers);
   console.log(filteredUsers);
   const allUsers = userJson;
 
-  const usersToDisplay = filteredUsers.length > 0 ? filteredUsers : allUsers;
 
+  const paginate = (array, page_size, page_number) => {
+    return array.slice((page_number - 1) * page_size, page_number * page_size);
+  };
+
+  const usersToDisplay = filteredUsers.length > 0 ? paginate(filteredUsers, perPage, currentPage) : paginate(allUsers, perPage, currentPage);
+
+  const totalPages = Math.ceil((filteredUsers.length > 0 ? filteredUsers.length : allUsers.length) / perPage);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
   return (
-    <div className="items-center p-0 overflow-auto">
+    <div className="items-center p-0 overflow-auto h-lvh">
       <div className="mt-1 -mb-3">
         <div className="not-prose relative  overflow-hidden dark:bg-slate-700/75">
           <div className="absolute inset-0 bg-grid-slate-100 [mask-image:linear-gradient(0deg,#fff,rgba(255,255,255,0.6))] dark:bg-grid-slate-700/25 dark:[mask-image:linear-gradient(0deg,rgba(255,255,255,0.1),rgba(255,255,255,0.5))]"></div>
@@ -47,9 +59,36 @@ const UserTable = () => {
               </table>
             </div>
           </div>
-          <div className="absolute inset-0 pointer-events-none border border-black/5 rounded-xl dark:border-white/5"></div>
+          <nav aria-label="Page navigation example">
+            <ul className="list-style-none flex">
+              <li>
+                <button
+                  className="relative block rounded bg-transparent px-3 py-1.5 text-sm text-surface transition duration-300 hover:bg-neutral-100 focus:bg-neutral-100 focus:text-primary-700 focus:outline-none active:bg-neutral-100 active:text-primary-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:focus:text-primary-500 dark:active:bg-neutral-700 dark:active:text-primary-500"
+                  onClick={() => handlePageChange(currentPage - 1)}
+                  disabled={currentPage === 1}
+                >&laquo;</button>
+              </li>
+              {[...Array(totalPages).keys()].map((pageNumber) => (
+                <li key={pageNumber}>
+                  <button
+                    className={`relative block rounded bg-transparent px-3 py-1.5 text-sm text-surface transition duration-300 hover:bg-neutral-100 focus:bg-neutral-100 focus:text-primary-700 focus:outline-none active:bg-neutral-100 active:text-primary-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:focus:text-primary-500 dark:active:bg-neutral-700 dark:active:text-primary-500 ${pageNumber + 1 === currentPage ? 'bg-blue-500 text-white' : ''}`}
+                    onClick={() => handlePageChange(pageNumber + 1)}
+                  >{pageNumber + 1}</button>
+                </li>
+              ))}
+              <li>
+                <button
+                  className="relative block rounded bg-transparent px-3 py-1.5 text-sm text-surface transition duration-300 hover:bg-neutral-100 focus:bg-neutral-100 focus:text-primary-700 focus:outline-none active:bg-neutral-100 active:text-primary-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:focus:text-primary-500 dark:active:bg-neutral-700 dark:active:text-primary-500"
+                  onClick={() => handlePageChange(currentPage + 1)}
+                  disabled={currentPage === totalPages}
+                >&raquo;</button>
+              </li>
+            </ul>
+          </nav>
         </div>
+
       </div>
+
     </div>
   );
 }
